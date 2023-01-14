b4919b397189154dacbd516768c78618c41b5723
lkearn <liam@sproutlabs.com.au>
2023-01-14 23:59:15 +1100

    Feat: Add random and clamping functions to math utils.

--- src/math_util.h
+++ src/math_util.h
@@ -1,7 +1,22 @@
 #pragma once
 
 #include <math.h>
+#include <stdlib.h>
 
 static inline double degrees_to_radians(double degrees) {
     return degrees * M_PI / 180.0;
+}
+
+static inline double rand_double(void) { return rand() / (RAND_MAX + 1.0); }
+
+static inline double rand_double_between(double min, double max) {
+    return min + (max - min) * rand_double();
+}
+
+static inline double clamp(double value, double min, double max) {
+    if (value < min)
+        return min;
+    if (value > max)
+        return max;
+    return value;
 }
